f8c387512e2d265b351794ddfb919d94424ac8a4
Randy Treit <randyt@outlook.com>
2020-09-15 21:51:27 -0700

    Clean up code.

--- list_to_code.py
+++ list_to_code.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import csv
 import os
+
 path = "d:/data/"
 sourcefile = "data.txt"
 outfile = "out.txt"
 source_location = f"{path}/{sourcefile}"
 output_location = f"{path}/{outfile}"
 if os.path.exists(source_location):
-    lines = pd.read_csv(f"{path}/{sourcefile}", sep='\t', header=None)
-    fields = lines.iloc[:,0]
+    lines = pd.read_csv(f"{path}/{sourcefile}", sep="\t", header=None)
+    fields = lines.iloc[:, 0]
     if os.path.exists(output_location):
         os.remove(output_location)
     for i in lines[0].values:
-        line_text = f"Guid.Parse(\"{i}\"),\n"
-        with open(output_location,mode='a') as output_file:
+        line_text = f'Guid.Parse("{i}"),\n'
+        with open(output_location, mode="a") as output_file:
             output_file.write(line_text)
 else:
     print(f"{source_location} not found!")
-    
