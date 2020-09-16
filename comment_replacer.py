ea7587fb3c658704bd53bb0964ccbabfe1ae2bcd
Randy Treit <randyt@microsoft.com>
2020-09-16 15:36:14 -0700

    Handle whitespace and utf-16

--- comment_replacer.py
+++ comment_replacer.py
@@ -1,6 +1,7 @@
 import os
+import re
 
-target = "//\n// TODO: Add constructor logic here\n//\n"
+target = "\/\/\\n\s*\/\/\s*TODO:\s*Add constructor logic here\s*\\n\s*\/\/\s*\\n"
 print("Enter a path to mangle:")
 path = input()
 print(f"Searching for\n{target}\n")
@@ -10,13 +11,21 @@ for (root, dirs, files) in os.walk(path):
     for file in files:
         current_file = os.path.join(current_directory, file)
         if file.endswith(".cs"):
-            with open(current_file, "r+", encoding="utf-8") as f:
-                content = f.read()
-                if content.find(target) > 0:
-                    counter += 1
-                    print(f"Found match in {current_file} - replacing!")
-                    content = content.replace(target, "")
-                    f.seek(0)
-                    f.write(content)
-                    f.truncate()
+            try:
+                with open(current_file, "r+", encoding="utf-8") as f:
+                    try:
+                        content = f.read()
+                    except:
+                        with open(current_file, "r+", encoding="utf-16") as f:
+                            content = f.read()
+                    if re.search(target, content):
+                        counter += 1
+                        print(f"Found match in {current_file} - replacing!")
+                        content = re.sub(target, "", content)
+                        f.seek(0)
+                        f.write(content)
+                        f.truncate()
+            except Exception as e:
+                print(current_file)
+                print(e)
 print(f"Replaced matched content in {counter} files.")
