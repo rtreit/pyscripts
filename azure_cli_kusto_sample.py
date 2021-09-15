82a9f9ff406f504fecee3e4dd2d449e8d60c17b4
Randy Treit <38894017+rtreit@users.noreply.github.com>
2021-09-15 11:11:36 -0700

    Reformat

--- azure_cli_kusto_sample.py
+++ azure_cli_kusto_sample.py
@@ -10,11 +10,12 @@ from tabulate import tabulate
 
 profile = get_cli_profile()
 
+
 def query_kusto_rest(
-    query, 
-    kusto_cluster = "", # TODO: <cluster>.windef.kusto.net goes here
-    kusto_database = "", # TODO: kusto database goes here
-    kusto_tenant = "" # TODO: kusto tenant goes here
+    query,
+    kusto_cluster="",  # TODO: <cluster>.windef.kusto.net goes here
+    kusto_database="",  # TODO: kusto database goes here
+    kusto_tenant="",  # TODO: kusto tenant goes here
 ):
     cli_token = profile.get_access_token_for_resource(
         resource=f"https://{kusto_cluster}.kusto.windows.net",
@@ -25,39 +26,40 @@ def query_kusto_rest(
         "Authorization": f"bearer {cli_token}",
         "Accept": "application/json",
         "Content-Type": "application/json; charset=utf-8",
-        "Host": f"{kusto_cluster}.kusto.windows.net"
+        "Host": f"{kusto_cluster}.kusto.windows.net",
     }
 
-    params = {
-        "db": f"{kusto_database}",
-        "csl": f"{query}"
-
-    }
+    params = {"db": f"{kusto_database}", "csl": f"{query}"}
 
     url = f"https://{kusto_cluster}.kusto.windows.net/v1/rest/query"
     r = requests.get(url, headers=headers, params=params)
     result = r.json()
-    rows = result['Tables'][0]['Rows']
-    columns = result['Tables'][0]['Columns']
+    rows = result["Tables"][0]["Rows"]
+    columns = result["Tables"][0]["Columns"]
     columns[0]["ColumnName"]
     column_names = []
     for index in range(0, len(columns)):
-        column_names.append(columns[index]['ColumnName'])    
+        column_names.append(columns[index]["ColumnName"])
     table = json.dumps(rows)
     df = pd.read_json(StringIO(json.dumps(rows)))
     df.columns = column_names
     df = df.infer_objects()
     return df
 
+
 def main(cluster, db, tenant, query):
-    df = query_kusto_rest(kusto_cluster=cluster, kusto_database=db, kusto_tenant=tenant, query=query)
+    df = query_kusto_rest(
+        kusto_cluster=cluster, kusto_database=db, kusto_tenant=tenant, query=query
+    )
     print(
-            tabulate(df,
-                headers="keys",
-                tablefmt="simple",
-                showindex="never",
-            )
-        )    
+        tabulate(
+            df,
+            headers="keys",
+            tablefmt="simple",
+            showindex="never",
+        )
+    )
+
 
 if __name__ == "__main__":
     try:
